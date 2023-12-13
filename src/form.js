fe646791b0caa502e55868ff5b41507f66f6593c
JooMin Choi <joominchoi23@gmail.com>
2023-12-13 18:11:46 +0000

    Append input to labels and set for attribute for labels

--- src/form.js
+++ src/form.js
@@ -3,54 +3,64 @@ function createProjectForm() {
   form.id = 'form';
 
   const emailLabel = document.createElement('label');
+  emailLabel.htmlFor = 'email';
   emailLabel.textContent = 'Email: ';
   form.appendChild(emailLabel);
   const emailInput = document.createElement('input');
   emailInput.type = 'email';
   emailInput.id = 'email';
   emailInput.name = 'name';
-  form.appendChild(emailInput);
+  emailLabel.appendChild(emailInput);
+  form.appendChild(emailLabel);
 
   form.appendChild(document.createElement('br'));
 
   const countryLabel = document.createElement('label');
+  countryLabel.htmlFor = 'country';
   countryLabel.textContent = 'Country: ';
-  form.appendChild(countryLabel);
   const countryInput = document.createElement('input');
   countryInput.type = 'text';
   countryInput.id = 'country';
   countryInput.name = 'country';
-  form.appendChild(countryInput);
+  countryLabel.appendChild(countryInput);
+  form.appendChild(countryLabel);
+
   form.appendChild(document.createElement('br'));
 
   const zipLabel = document.createElement('label');
+  zipLabel.htmlFor = 'zip';
   zipLabel.textContent = 'ZIP Code: ';
-  form.appendChild(zipLabel);
   const zipInput = document.createElement('input');
   zipInput.type = 'text';
   zipInput.id = 'zip';
   zipInput.name = 'zip';
-  form.appendChild(zipInput);
+  zipLabel.appendChild(zipInput);
+  form.appendChild(zipLabel);
+
   form.appendChild(document.createElement('br'));
 
   const passwordLabel = document.createElement('label');
+  passwordLabel.htmlFor = 'password';
   passwordLabel.textContent = 'Password: ';
-  form.appendChild(passwordLabel);
   const passwordInput = document.createElement('input');
   passwordInput.type = 'password';
   passwordInput.id = 'password';
   passwordInput.name = 'password';
-  form.appendChild(passwordInput);
+  passwordLabel.appendChild(passwordInput);
+  form.appendChild(passwordLabel);
+
   form.appendChild(document.createElement('br'));
 
   const confirmPasswordLabel = document.createElement('label');
+  confirmPasswordLabel.htmlFor = 'confirm-password';
   confirmPasswordLabel.textContent = 'Confirm Password: ';
-  form.appendChild(confirmPasswordLabel);
   const confirmPasswordInput = document.createElement('input');
   confirmPasswordInput.type = 'password';
   confirmPasswordInput.id = 'confirm-password';
   confirmPasswordInput.name = 'confirm-password';
-  form.appendChild(confirmPasswordInput);
+  confirmPasswordLabel.appendChild(confirmPasswordInput);
+  form.appendChild(confirmPasswordLabel);
+
   form.appendChild(document.createElement('br'));
 
   const submitButton = document.createElement('button');
