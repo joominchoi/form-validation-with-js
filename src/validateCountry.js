23a3d50424552885c22ffa15a5acb892a456da78
JooMin Choi <joominchoi23@gmail.com>
2023-12-16 23:31:53 +0000

    Refactor country validation module

--- src/validateCountry.js
+++ src/validateCountry.js
@@ -4,10 +4,6 @@ function validateCountry(countryInput) {
 
   if (emailValue === '') {
     errorMessage.textContent = 'You need to enter a country.';
-  // } else if (!emailRegex.test(emailValue)) {
-  //   errorMessage.textContent = 'Entered value needs to be an email address.';
-  // } else if (emailValue.length < emailInput.minLength) {
-  //   errorMessage.textContent = `Email should be at least ${emailInput.minLength} characters; you entered ${emailValue.length}.`;
   } else {
     errorMessage.textContent = '';
   }
